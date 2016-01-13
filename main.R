ff1b1b7a3d188b973df97fa60f615b2281b3ab45
TheLeopards <georgios.anastasiou@wur.nl>
2016-01-13 18:29:57 +0100

    Added more documentation of the steps

--- main.R
+++ main.R
@@ -29,7 +29,7 @@ opar <- par(mfrow=c(3,2))
 for(i in 1:nlayers(alldata[[1:6]])){
 	plot(alldata[[i]], alldata$VCF, pch = ".", col = "darkgreen", main= "Relationship between Landsat bands and VCF")
 	}
-
+par(opar)
 
 # getting rid of values above 100 which represent clouds etc. (keeping just forest cover)
 vcfGewata[vcfGewata > 100] <- NA
@@ -41,14 +41,16 @@ model <- lm(VCF ~ band1 + band2 + band3 + band4 + band5 + band7, data = df, impo
 #summary(alldata$VCF)
 summary(model)
 
+# prediction using model
 predVCF <- predict(alldata, model=model, na.rm=TRUE)
 summary(predVCF)
 
+# plotting prediction and original
 opar <- par(mfrow=c(1,2))
 plot(predVCF, main="Predicted forest cover", zlim=c(0,100))
 plot(vcfGewata, main="Original forest cover")
 
-
+#RMSE for VCF
 RMSE <- sqrt(mean((alldata$VCF-predVCF)^2))
 par(opar)
 plot(RMSE)
