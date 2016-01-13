723fb482f60417ca64e407abf64ee1c93c7d2e0f
TheLeopards <georgios.anastasiou@wur.nl>
2016-01-13 17:13:01 +0100

    Getting started we created a brick of the bands we are going to use and gave them a more intuitive name. Then converted the the brick so that we can use it for the prediction model. Plotted the model next to the original actual VCF for comparison. Then started the RMSE calculation between the predicted and the actual VCF.

---
+++ main.R
@@ -0,0 +1,48 @@
+
+
+
+
+library(raster)
+
+
+load("data/GewataB1.rda")
+load("data/GewataB2.rda")
+load("data/GewataB3.rda")
+load("data/GewataB4.rda")
+load("data/GewataB5.rda")
+load("data/GewataB7.rda")
+load("data/vcfGewata.rda")
+## band 6 (thermal infra-red) will be excluded from this exercise
+
+
+
+## build a brick containing all data
+alldata <- brick(GewataB1, GewataB2, GewataB3, GewataB4, GewataB5, GewataB7, vcfGewata)
+names(alldata) <- c("band1", "band2", "band3", "band4", "band5", "band7", "VCF")
+
+## extract all data to a data.frame
+df <- as.data.frame(getValues(alldata))
+
+# getting rid of values above 100 which represent clouds etc. (keeping just forest cover)
+vcfGewata[vcfGewata > 100] <- NA
+
+# model
+model <- lm(VCF ~ band1 + band2 + band3 + band4 + band5 + band7, data = df, importance=TRUE)
+
+#summary(alldata$VCF)
+summary(model)
+
+predVCF <- predict(alldata, model=model, na.rm=TRUE)
+summary(predVCF)
+
+opar <- par(mfrow=c(1,2))
+plot(predVCF, main="Predicted forest cover", zlim=c(0,100))
+plot(vcfGewata, main="Original forest cover")
+
+
+RMSE <- sqrt(mean((alldata$VCF-predVCF)^2))
+par(opar)
+plot(RMSE)
+
+
+
