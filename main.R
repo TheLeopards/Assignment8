cb9ff010d2476c7a21d6cf81cf0708d53b4f1e33
TheLeopards <georgios.anastasiou@wur.nl>
2016-01-14 15:46:56 +0100

    Final improvements and submission

--- main.R
+++ main.R
@@ -14,6 +14,9 @@ load("data/GewataB7.rda")
 load("data/vcfGewata.rda")
 load("data/trainingPoly.rda")
 
+# getting rid of values above 100 representing classes other than forest
+vcfGewata[vcfGewata > 100] <- NA
+
 ## building a brick containing all data
 alldata <- brick(GewataB1, GewataB2, GewataB3, GewataB4, GewataB5, GewataB7, vcfGewata)
 names(alldata) <- c("band1", "band2", "band3", "band4", "band5", "band7", "VCF")
@@ -25,11 +28,10 @@ df <- as.data.frame(getValues(alldata))
 opar <- par(mfrow=c(3,2))
 for(i in 1:nlayers(alldata[[1:6]])){
 	plot(alldata[[i]], alldata$VCF, pch = ".", col = "darkgreen", main= "Relationship between Landsat bands and VCF")
-	}
+	warnings()
+		}
 par(opar)
 
-# getting rid of values above 100 representing classes other than forest
-vcfGewata[vcfGewata > 100] <- NA
 
 # model
 model <- lm(VCF ~ band1 + band2 + band3 + band4 + band5 + band7, data = df)
@@ -42,10 +44,10 @@ predVCF <- predict(alldata, model=model, na.rm=TRUE)
 opar <- par(mfrow=c(1,2))
 plot(predVCF, main="Predicted forest cover", zlim=c(0,100))
 plot(vcfGewata, main="Original forest cover")
+par(opar)
 
 # calculating RMSE
 RMSE <- sqrt(mean((alldata$VCF-predVCF)^2))
-par(opar)
 plot(RMSE)
 
 # calculating RMSE for trainingpolygons
@@ -57,7 +59,6 @@ RMSE_classes <- zonal(RMSE, classes, fun=mean, na.rm=TRUE)
 	# editing resulting matrix for analysis
 RMSE_classes <- cbind(RMSE_classes, c("cropland","forest", "wetland"))
 colnames(RMSE_classes) <- c("Zone", "RMSE", "Class")
-
 
 
 
